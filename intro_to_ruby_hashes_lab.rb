b31bee4c691b3df69822824b310c7d696d11f664
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 17:21:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,8 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
-end
+  :hash
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
