4ee18f2c92e7d0e541116f3d09911551395197d4
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 18:06:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,12 +19,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  if hash[:key]
-    puts
-  else 
     hash = {:key => 'value'}
     puts :key
-  end
 end
 
 def update_counting_hash(hash, key)
