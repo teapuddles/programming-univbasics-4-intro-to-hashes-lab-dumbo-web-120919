8a34aab22b77885d1d907607efe94cfacef36abf
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 17:15:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,9 +15,9 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  key = :name
-  value = "Grace Hopper"
-  my_creation = {:name => "Grace Hopper"}
+  key = :key
+  value = "value"
+  my_creation = {:key => "value"}
   return my_creation
 end
 
