e5075f9055263576ec03e8f6ea8c937c82db8ac8
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 18:43:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- update_hash = update_counting_hash
- if update_hash[key] += 1 
+ if hash[key] += 1 
  else
-   udate_hash[key] = 1
+   hash[key] = 1
  end
 end
